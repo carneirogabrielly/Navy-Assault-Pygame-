d9d8761140ea02f00de0b363edfee431cf075439
Venanciofaf <venanciofaf@al.insper.edu.br>
2023-05-22 11:21:41 -0300

    Commit da tela de fundo do game

---
+++ jogo_total.py
@@ -0,0 +1,34 @@
+import pygame 
+import math 
+
+pygame.init() #Inicializa o framework do pygame 
+
+#---Gera tela principal 
+largura = 650 # Largura da tela 
+comprimento = 800 #Comprimento da tela 
+window = pygame.display.set_mode((largura , comprimento)) #Inicializa o display da janela, com as suas dimensões 
+pygame.display.set_caption('Navy Assault') #Coloca o título da janela 
+
+#----Inicializa estrutura de dados 
+game = True
+# ---- Inicia assets (Imagem) 
+imagem_fundo = pygame.image.load('Imagens/tile_0084_water11.png').convert() #Inicializa a imagem no pygame 
+imagem_fundo = pygame.transform.scale(imagem_fundo , (650,800)).convert() #Converte a imagem para a escala 
+
+
+#----Loop principal do jogo ---
+while game: 
+    
+    # ----- Trata eventos
+    for event in pygame.event.get(): #pygame.evente.get devolve uma lista com todos os eventos que ocorreram desde a última janela 
+        # ----- Verifica consequências
+        if event.type == pygame.QUIT: #Se o comando do evento for igual a pygame.quit, o loop acaba 
+            game = False
+
+    #Gera saídas 
+    window.fill( (0 , 0 , 0)) #Colore a janela window com tudo em branco 
+    window.blit(imagem_fundo , (0,0))   #Posiciona a imagem de fundo na janela window, na posição 0,0
+    #Autaliza estado do jogo 
+    pygame.display.update() #Atualiza o estado do jogo observado a cada loop
+#--- Finalização 
+pygame.quit() #Finaliza o game 
