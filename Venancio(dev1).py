9befe7ebcd9509b21de7526ad63e4714e4e79ee5
Venanciofaf <venanciofaf@al.insper.edu.br>
2023-05-23 18:14:07 -0300

    Rascunho Movimento jogado Venâncio

--- Venancio(dev1).py
+++ Venancio(dev1).py
@@ -1,6 +1,9 @@
+from typing import Any
 import pygame 
 import math 
-import random 
+import random
+
+
 pygame.init() #Inicializa o framework do pygame 
 
 #---Gera tela principal 
@@ -12,16 +15,46 @@ pygame.display.set_caption('Navy Assault') #Coloca o título da janela
 
 
 # ---- Inicia assets (Imagem) 
+#Dimensões das imagens 
 largura_oponente = 130  
 comprimento_oponente = 160
+largura_jogador = 180
+comprimento_jogador = 210
+#Imagens
 imagem_fundo = pygame.image.load('Imagens/Fundo.png').convert() #Inicializa a imagem no pygame 
 imagem_fundo = pygame.transform.scale(imagem_fundo , (650,800)) #Converte a imagem para a escala 
+
 imagem_oponente = pygame.image.load('Imagens/Barco_inimigo/Barco_inimigo.png').convert_alpha()
 imagem_oponente = pygame.transform.scale(imagem_oponente , (largura_oponente,comprimento_oponente))
 
+imagem_jogador =  pygame.image.load('Imagens/Barco_jogador/Barco/Barco_amigo.png').convert_alpha()
+imagem_jogador = pygame.transform.scale(imagem_jogador , (largura_jogador,comprimento_jogador))
 
 #----Inicializa estrutura de dados 
 game = True
+
+#Classe do jogador 
+class Player(pygame.sprite.Sprite):
+    def __init__(self , imagem_jogador):
+        pygame.sprite.Sprite.__init__(self)
+        
+        self.image = imagem_jogador
+        self.rect = self.image.get_rect()
+        self.rect.centerx = largura/2
+        self.rect.bottom = comprimento - 20
+        self.vxj = 0
+        self.vyj = 0
+    def update(self):
+        self.rect.x += self.vxj
+        self.rect.y += self.vyj
+        if self.rect.left < 0: 
+            self.rect.left = 1 
+        if self.rect.right > largura:
+            self.rect.right = largura - 10
+        if self.rect.top > comprimento:
+            self.rect.y = comprimento
+        if self.rect.y > comprimento - 150:
+            self.rect.y = comprimento - 150
 
 #Classe do navio inimigo 
 class Inimigo(pygame.sprite.Sprite): #Classe dos navios inimigos 
@@ -39,23 +72,26 @@ class Inimigo(pygame.sprite.Sprite): #Classe dos navios inimigos
         self.rect.x += self.vx_oponente 
         self.rect.y += self.vy_oponente
         #Condições para reposcionar o inimigo: 
-        if self.rect.x > largura:
+        if self.rect.left > largura:
             self.rect.x = largura - 10 
             self.vx_oponente = self.vx_oponente * -1
-        if self.rect.x <  0:
-            self.rect.x = 1 
+        if self.rect.right <  0:
+            self.rect.x = 0
             self.vx_oponente = self.vx_oponente * -1 
-        if self.rect.y > comprimento:
+        if self.rect.top > comprimento:
             self.rect.y = 1
             self.rect.x = random.randint(0 , 650)
 
 
 #criando navios: 
-navios_inimigo = pygame.sprite.Group() #É uma lista com mais funcionalidades
+todos_elementos  = pygame.sprite.Group() #É uma lista com mais funcionalidades
 n_inimigos = 4
 for i in range(n_inimigos):
     navio = Inimigo(imagem_oponente)
-    navios_inimigo.add(navio)
+    todos_elementos.add(navio)
+#player 
+player = Player(imagem_jogador)
+todos_elementos.add(player)
 
 
 #Relógio que controla o loop
@@ -70,11 +106,32 @@ while game:
         # ----- Verifica consequências
         if event.type == pygame.QUIT: #Se o comando do evento for igual a pygame.quit, o loop acaba 
             game = False
-    navios_inimigo.update() #Atualizando a posição do inimigo 
+        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_LEFT:
+                player.vxj -= 8
+            if event.type == pygame.K_RIGHT:
+                player.vxj += 8
+            if event.type == pygame.K_UP:
+                player.vyj -= 4
+            if event.type == pygame.K_DOWN:
+                player.vyj += 4
+        
+        if event.type == pygame.KEYUP:
+            # Dependendo da tecla, altera a velocidade.
+            if event.key == pygame.K_LEFT:
+                player.vxj += 8/2
+            if event.key == pygame.K_RIGHT:
+                player.vxj -= 8/2
+            if event.key == pygame.K_UP:
+                player.vyj += 4/2
+            if event.key == pygame.K_DOWN:
+                player.vyj -= 4/2
+        
+    todos_elementos.update() #Atualizando a posição do inimigo 
     #Gera saídas 
     window.fill( (0 , 0 , 0)) #Colore a janela window com tudo em branco 
     window.blit(imagem_fundo , (0,0))   #Posiciona a imagem de fundo na janela window, na posição 0,0
-    navios_inimigo.draw(window)
+    todos_elementos.draw(window)
     #Autaliza estado do jogo 
     pygame.display.update() #Atualiza o estado do jogo observado a cada loop
 #--- Finalização 
