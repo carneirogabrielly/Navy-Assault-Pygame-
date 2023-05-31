7ec99d3eff4ea963b503165bcba1bbe5bbf842b7
João Braga <joaovab@al.insper.edu.br>
2023-05-31 11:13:13 -0300

    separando em arquivos

--- jogo.py
+++ jogo.py
@@ -1,7 +1,22 @@
+#---- Iniciando o jogo ---- #
+# ---- realizando imports necessários
 import pygame
 import random
-
 from tela_jogo import tela_jogo
 from config import largura , comprimento 
 
-pygame 
+pygame.init()
+pygame.mixer.init()
+
+#Gera tela principal
+
+window = pygame.display.set_mode((largura, comprimento))
+pygame.display.set_caption('Navy Assault')
+
+
+tela_jogo(window)
+
+
+#finaliza
+
+pygame.quit()
