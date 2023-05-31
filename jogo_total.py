a88e86d22d0a3df64d646ae6d89656f391445e61
wellingtonrs07 <wr24349@gmail.com>
2023-05-31 09:44:16 -0300

    jogo total atualizado e com sons

--- jogo_total.py
+++ jogo_total.py
@@ -44,53 +44,73 @@ def carrega_imagens():
     assets = {}
     assets['imagem_fundo'] = pygame.image.load('Imagens/Fundo.png').convert() #Inicializa a imagem no pygame 
     assets['imagem_fundo'] = pygame.transform.scale(assets['imagem_fundo'] , (650,800)) #Converte a imagem para a escala 
+    
+    #imagem tela inicio
+    assets['imagem_inicial'] = pygame.image.load('Imagens/tela_inicio.png').convert() #Inicializa a imagem no pygame
+    assets['imagem_inicial'] = pygame.transform.scale(assets['imagem_inicial'] , (650,800)) #Converte a imagem para a escala 
+    
+    
+    #imagem tela instruções
+    assets['imagem_instrucoes'] = pygame.image.load('Imagens/instruções.png').convert() #Inicializa a imagem no pygame
+    assets['imagem_instrucoes'] = pygame.transform.scale(assets['imagem_instrucoes'] , (650,800)) #Converte a imagem para a escala 
+    
 
-    assets['imagem_inicial'] = pygame.image.load('Imagens/tela_inicio.png').convert()
-    assets['imagem_inicial'] = pygame.transform.scale(assets['imagem_inicial'] , (650,800))
+    #imagem tela de game over
+    assets['game_over'] = pygame.image.load('Imagens/tela_game_over.png').convert() #Inicializa a imagem no pygame
+    assets['game_over'] = pygame.transform.scale(assets['game_over'], (650,800)) #Converte a imagem para a escala 
+    
 
-    assets['game_over'] = pygame.image.load('Imagens/tela_game_over.png').convert()
-    assets['game_over'] = pygame.transform.scale(assets['game_over'], (650,800))
+    #imagem tela de vitória
+    assets['tela_vitoria'] = pygame.image.load('Imagens/tela_vitoria.png') #Inicializa a imagem no pygame
+    assets['tela_vitoria'] = pygame.transform.scale(assets['tela_vitoria'], (650,800)) #Converte a imagem para a escala 
+    
 
-    assets['tela_vitoria'] = pygame.image.load('Imagens/tela_vitoria.png')
-    assets['tela_vitoria'] = pygame.transform.scale(assets['tela_vitoria'], (650,800))
+    #imagem da embarcação do oponente
+    assets['imagem_oponente'] = pygame.image.load('Imagens/Barco_inimigo/Barco_inimigo.png').convert_alpha() #Inicializa a imagem no pygame
+    assets['imagem_oponente'] = pygame.transform.scale(assets['imagem_oponente'] , (largura_oponente,comprimento_oponente)) #Converte a imagem para a escala 
 
-    assets['imagem_oponente'] = pygame.image.load('Imagens/Barco_inimigo/Barco_inimigo.png').convert_alpha()
-    assets['imagem_oponente'] = pygame.transform.scale(assets['imagem_oponente'] , (largura_oponente,comprimento_oponente))
+    #imagem da embarcação do jogador
+    assets['imagem_jogador'] = pygame.image.load('Imagens/Barco_jogador/Barco/Barco_amigo.png').convert_alpha() #Inicializa a imagem no pygame
+    assets['imagem_jogador'] = pygame.transform.scale(assets['imagem_jogador'] , (largura_jogador,comprimento_jogador)) #Converte a imagem para a escala 
 
-    assets['imagem_jogador'] = pygame.image.load('Imagens/Barco_jogador/Barco/Barco_amigo.png').convert_alpha()
-    assets['imagem_jogador'] = pygame.transform.scale(assets['imagem_jogador'] , (largura_jogador,comprimento_jogador))
+    #imagem do tiro do jogador
+    assets['imagem_tiro'] = pygame.image.load('Imagens/Barco_jogador/Canhões/Canhões_jogador/Segundo_canhã_jogador/Tiro_canhão2.png') #Inicializa a imagem no pygame
+    assets['imagem_tiro'] = pygame.transform.scale(assets['imagem_tiro'] , (largura_tiro , comrpimento_tiro) ) #Converte a imagem para a escala 
 
-    assets['imagem_tiro'] = pygame.image.load('Imagens/Barco_jogador/Canhões/Canhões_jogador/Segundo_canhã_jogador/Tiro_canhão2.png')
-    assets['imagem_tiro'] = pygame.transform.scale(assets['imagem_tiro'] , (largura_tiro , comrpimento_tiro) )
-
-    assets['imagem_tiro_inimigo'] = pygame.image.load('Imagens/Barco_jogador/Canhões/Canhões_jogador/Quarto_canhão_jogador/Tiro_canhão4.png')
-    assets['imagem_tiro_inimigo'] = pygame.transform.scale(assets['imagem_tiro_inimigo'] , (largura_tiro , comrpimento_tiro))
-
-    assets['imagem_placar'] = pygame.image.load('Imagens/Foto_placar.png')
-    assets['imagem_placar'] = pygame.transform.scale(assets['imagem_placar'] , (100, 100))
-
-    assets['Imagem_vida'] = pygame.transform.scale(assets['imagem_jogador'], (largura_vida, comprimento_vida))
-
-
-
-    assets['imagem_boss']  = pygame.image.load('Imagens/Boss/Boss.png')
-    assets['imagem_boss'] = pygame.transform.scale(assets['imagem_boss'] , (largura_boss , comprimento_boss))
-
-    assets['imagem_bala_boss'] = pygame.image.load('Imagens/Boss/Bala_de_canhão_boss.png')
-    assets['imagem_bala_boss']  = pygame.transform.scale(assets['imagem_bala_boss'] , (largura_bala_boss , comprimento_bala_boss))
+    #imagem do tiro inimigo
+    assets['imagem_tiro_inimigo'] = pygame.image.load('Imagens/Barco_jogador/Canhões/Canhões_jogador/Quarto_canhão_jogador/Tiro_canhão4.png') #Inicializa a imagem no pygame
+    assets['imagem_tiro_inimigo'] = pygame.transform.scale(assets['imagem_tiro_inimigo'] , (largura_tiro , comrpimento_tiro)) #Converte a imagem para a escala 
 
 
+    #imagem do placar que será printado
+    assets['imagem_placar'] = pygame.image.load('Imagens/Foto_placar.png') #Inicializa a imagem no pygame
+    assets['imagem_placar'] = pygame.transform.scale(assets['imagem_placar'] , (100, 100)) #Converte a imagem para a escala 
 
+    #imagem do barco que representa a imagem do jogador
+    assets['Imagem_vida'] = pygame.transform.scale(assets['imagem_jogador'], (largura_vida, comprimento_vida)) #Converte a imagem para a escala 
+
+    #imagem do boss que aparece ao final
+    assets['imagem_boss']  = pygame.image.load('Imagens/Boss/Boss.png') #Inicializa a imagem no pygame
+    assets['imagem_boss'] = pygame.transform.scale(assets['imagem_boss'] , (largura_boss , comprimento_boss)) #Converte a imagem para a escala 
+
+    #imagem do tiro de canhão do boss
+    assets['imagem_bala_boss'] = pygame.image.load('Imagens/Boss/Bala_de_canhão_boss.png')  #Inicializa a imagem no pygame
+    assets['imagem_bala_boss']  = pygame.transform.scale(assets['imagem_bala_boss'] , (largura_bala_boss , comprimento_bala_boss)) #Converte a imagem para a escala 
+
+    #lista vazia da animação do tiro
     anim_tiro_jogador = []
 
+    #armazenar a imagem na lista
     for i in range(3):
         arquivo = f'Imagens/Barco_jogador/Barco/anim_barco_jogador_{i}.png '
         imagem = pygame.image.load(arquivo).convert_alpha()
         imagem = pygame.transform.scale(imagem,(largura_jogador,comprimento_jogador))
         anim_tiro_jogador.append(imagem)
 
+    #adicionando ao dicionário
     assets['anim_tiro_jogador'] = anim_tiro_jogador
 
+    #fazendo animações da explosão do barco do jogador
     anim_explosion_barco_amigo = []
     for i in range(27):
         arquivo2 = f'Imagens/Barco_jogador/Barco/animação-destruição/{i}.png'
@@ -99,8 +119,10 @@ def carrega_imagens():
         imagem = pygame.transform.scale(imagem,(110,110))
         anim_explosion_barco_amigo.append(imagem)
 
+    #imagem sendo adicionada ao dicionário
     assets['anim_explosion_barco_amigo'] = anim_explosion_barco_amigo
 
+    #criação da lista vazia para adicionar a animação da explosão do inimigo
     anim_explod_inimigo = []
 
     for i in range(23):
@@ -168,12 +190,13 @@ def carrega_imagens():
 
     assets['boss_chegando'] = pygame.mixer.Sound('Sons/som_boss_chegando.flac')
     assets['boss_chegando'].set_volume(0.9)
-    
+
     assets['matou boss'] = pygame.mixer.Sound('Sons/win_sound.wav')
     assets['matou boss'].set_volume(0.9)
 
     assets['boss atirando'] = pygame.mixer.Sound('Sons/son_explod_boss.wav')
     assets['boss atirando'].set_volume(0.3)
+    
     return assets
 
 assets = carrega_imagens()
@@ -638,11 +661,12 @@ def tela_jogo(window):
     perdeu_vidas = 3
     regenerando = 4
     playing = 2
-    tela_inicio = 0
+    tela_inicial = 0
+    tela_instrucoes = 1
     game_over = 6
     matou_boss = 7
     venceu = 8
-    state = tela_inicio
+    state = tela_inicial
 
     #Estados do estágio do jogo 
     fase_1 = 10
@@ -652,10 +676,26 @@ def tela_jogo(window):
     pygame.mixer.music.play(loops=-1)
     while state != game_over and state != venceu and state != acabou: 
         clock.tick(FPS) 
-        while state == tela_inicio:
+        while state == tela_inicial:
             window.fill( (0 , 0 , 0)) #Colore a janela window com tudo em branco 
             window.fill( (0 , 0 , 0))
             window.blit(assets['imagem_inicial'], imagem_fundo_rect)
+            
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        state = tela_instrucoes
+
+
+                if event.type == pygame.QUIT:
+                    state = acabou
+
+        while state == tela_instrucoes:
+            window.fill( (0 , 0 , 0)) #Colore a janela window com tudo em branco 
+            window.fill( (0 , 0 , 0))
+            window.blit(assets['imagem_instrucoes'], imagem_fundo_rect)
             
             pygame.display.update()
 
@@ -772,7 +812,7 @@ def tela_jogo(window):
                 state = venceu
         
         
-        if tempo_fase1 - (1000 * 90) > 0 and status_fase == fase_1:
+        if tempo_fase1 - (1000 * 60) > 0 and status_fase == fase_1:
             status_fase = fase_final
             assets['boss_chegando'].play()
             for tiro_inimigo2 in todos_tiros_inimigo:
@@ -792,7 +832,6 @@ def tela_jogo(window):
             if state == playing:
                 hits3 =  pygame.sprite.spritecollide(navio_amigo , todos_boss , False , pygame.sprite.collide_mask)
                 if len(hits3) > 0:
-                    assets['jogador colidindo'].play()
                     vidas -= 1 
                     if vidas > 0:
                         state = regenerando
@@ -807,14 +846,12 @@ def tela_jogo(window):
                         vidas_boss -= 1
                         boss_atingido = pygame.time.get_ticks()
                         if vidas_boss == 0:
-                            
                             chave.kill()
                             state = matou_boss
                         for termo in valor: 
                             termo.kill()
                 hits5 = pygame.sprite.groupcollide(todos_amigo , groups['todos_tiros_boss'] , False , True , pygame.sprite.collide_mask)
                 if len(hits5) > 0:
-                    assets['jogador colidindo'].play()
                     for key, value in hits5.items():
                         vidas -= 1 
                         if vidas > 0:
@@ -857,7 +894,6 @@ def tela_jogo(window):
             window.blit(regen , (largura/2 - 100 , 30))
         
 
-        print(state)
             #Autaliza estado do jogo 
         pygame.display.update() #Atualiza o estado do jogo observado a cada loop
     #--- Finalização 
